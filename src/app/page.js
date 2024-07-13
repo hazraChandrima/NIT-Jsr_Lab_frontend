0aa77afe317053898d8354cabb8a9584d34204db
DEVDUTTSAHOO30 <2023ugcs032@nitjsr.ac.in>
2024-07-13 14:49:07 +0530

    Added the Gallery section on Home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,17 @@ import MovingCardsSection from "@/components/HomeCompPritam/MovingImageSection/S
 import NewsSection from "@/components/HomeCompPritam/NewsComponents/NewsSection";
 import ParallaxImageComponent from "@/components/HomeCompPritam/ParallaxSection/ParallaxImageComponent";
 import TeamSection from "@/components/HomeCompPritam/TeamSection/Section";
-
+import Gallery from "@/components/HomeCompPritam/Gallery/GallerySection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <div className="h-dvh bg-slate-800">
-
-    </div>
-    <NewsSection/>
-    <ParallaxImageComponent/>
-    <TeamSection/>
-    
+      <div className="h-dvh bg-slate-800"></div>
+      <NewsSection />
+      <ParallaxImageComponent />
+      <TeamSection />
+      <Gallery />
     </>
   );
 }
