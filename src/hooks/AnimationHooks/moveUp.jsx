1e76825ea95c8f7cdd42f7866f63fbdc8d402357
Chandrima Hazra <hazrachandrima6@gmail.com>
2024-07-16 22:53:05 +0530

    changed some folder names and added animation hook

---
+++ src/hooks/AnimationHooks/moveUp.jsx
@@ -0,0 +1,28 @@
+"use client"
+
+
+import { useEffect } from 'react';
+import { useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+const useAnimationHook = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start({
+        opacity: 1,
+        y: 0,
+        transition: { duration: 0.5 },
+      });
+    }
+  }, [controls, inView]);
+
+  return { ref, controls };
+};
+
+export default useAnimationHook;
