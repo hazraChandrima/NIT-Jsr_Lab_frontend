8d72cf90e927bafd76faf102b4d6424a5a0fac7e
Abhishek <jawanpuriaabhishek@gmail.com>
2024-07-18 16:32:16 +0530

    hero in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
-
+import Hero from "@/components/Hero/hero";
+import img1 from '../assets/images/nit_jsr.jpeg';
+import img2 from '../assets/images/old_acad.jpg';
 export default function Home() {
   return (
     <>
-    <div>Home page</div>
+    <Hero/>
     </>
   );
 }
