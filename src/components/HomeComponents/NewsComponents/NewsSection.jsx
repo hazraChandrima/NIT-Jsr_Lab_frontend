a27d0e34f2704624094af66c1b8270f142ecc48e
Pritam Roy <ratsdust4226@gmail.com>
2024-07-18 12:56:20 +0530

    added container stuff

--- src/components/HomeComponents/NewsComponents/NewsSection.jsx
+++ src/components/HomeComponents/NewsComponents/NewsSection.jsx
@@ -4,7 +4,7 @@ import ViewMore from '../ViewMoreComponent/ViewMoreComponent'
 
 function NewsSection() {
   return (
-    <div className='flex flex-col py-5 min-h-dvh bg-slate-900 text-white text-right items-end '>
+    <div className='flex flex-col py-5 min-h-dvh  text-white text-right items-end '>
       <h1 className='text-3xl relative right-3'>NEWS</h1>
       <div className="py-6 text-left flex flex-col items-center">
       <NewsCard/>
