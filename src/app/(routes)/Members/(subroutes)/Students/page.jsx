c7c8d8ff7ea6c7bf0ae64096c6f73ffd98bd7965
Pritam Roy <ratsdust4226@gmail.com>
2024-07-21 15:10:32 +0530

    members page done

--- src/app/(routes)/Members/(subroutes)/Students/page.jsx
+++ src/app/(routes)/Members/(subroutes)/Students/page.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import ProgramWiseCard from '../../_components/ProgramWiseCards'
+
 
 export default function page() {
     return (
-      <div>
-        Student Members
+      <main className='min-h-screen w-full flex flex-col items-center bg-gray-800 py-20 px-5'>
+        <div className='flex flex-col items-start w-full max-w-[1256px]'>
+            {/* //Program Based */}
+      <div className='w-full flex mb-16 flex-col gap-14'>
+        <div>
+        <h1 className='text-2xl text-white'>Doctoral Program</h1>
+        <div className='w-full h-[2px] bg-white'>
+          <div className='w-[15%] h-full bg-blue-500'>
+          </div>
+        </div>
+        </div>
+        <div className='flex flex-col gap-4'>
+        <ProgramWiseCard/>
+        <ProgramWiseCard/>
+        <ProgramWiseCard/>
+        <ProgramWiseCard/>
+        </div>
       </div>
+      
+      <div className='w-full flex flex-col gap-14'>
+        <div>
+        <h1 className='text-2xl text-white'>Masters Program</h1>
+        <div className='w-full h-[2px] bg-white'>
+          <div className='w-[15%] h-full bg-blue-500'>
+          </div>
+        </div>
+        </div>
+        <div className='flex flex-col gap-4'>
+        <ProgramWiseCard/>
+        <ProgramWiseCard/>
+        <ProgramWiseCard/>
+        </div>
+      </div>
+
+      {/* //Program Based */}
+        </div>
+      </main>
     )
   }
--- src/app/(routes)/Members/layout.jsx
+++ src/app/(routes)/Members/layout.jsx
@@ -1,12 +1,20 @@
 import Navbar from "@/components/Navbar/navbar"
+import ResearchHero from "../Research/_components/ResearchHero/ResearchHero";
 import Footer from "@/components/Footer/Footer"
+import Navigation from "./_components/Navigation";
+import BreadCrumbs from "@/components/BreadCrumbs/BreadCrumbs";
 
 export default function MemberLayout({children}) {
     return (
-    <div>
+    <div className="overflow-x-hidden ">
         <Navbar/>
+        <ResearchHero/>
+        <div className="text-sm sm:text-base bg-slate-800">
+        <BreadCrumbs/>
+        <Navigation/>
         {children}
         <Footer/>
+        </div>
     </div>
     )
 }
