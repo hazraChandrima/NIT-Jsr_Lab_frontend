6432434371e305bd8a27bbb5b47b16e79ad7c515
Pritam Roy <ratsdust4226@gmail.com>
2024-08-05 18:08:58 +0530

    api called in profile section

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'nitjsr.ac.in',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
