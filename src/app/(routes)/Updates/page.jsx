0573f6388c8116ea54ad24c74bc2ec41512322a3
Pritam Roy <ratsdust4226@gmail.com>
2024-08-08 19:02:10 +0530

    updates components backend integrated

--- src/app/(routes)/Updates/page.jsx
+++ src/app/(routes)/Updates/page.jsx
@@ -1,273 +1,35 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import NewsCard from "@/components/HomeComponents/NewsComponents/NewsCard";
 import { useNews } from "@/contexts/NewsContext";
 import BreadCrumbs from "@/components/BreadCrumbs/BreadCrumbs";
-
-const newsData = [
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/12/2024",
-    title: "News Title 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-  {
-    date: "7/11/2024",
-    title: "News Title 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur saepe non, eaque fugiat quod eveniet quaerat quam, sit ratione tenetur quo aliquam.",
-    viewMoreLink: "/#",
-    galleryLink: "/#",
-  },
-];
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 const ITEMS_PER_PAGE = 5;
 
 function NewsPage() {
+  const [notices, setNotices] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+      const response = await fetch(
+        `https://cozy-captain-963d285ad5.strapiapp.com/api/notices?populate=Pdf&pagination[page]=${currentPage}&pagination[pageSize]=${ITEMS_PER_PAGE}`
+      );
+      const res = await response.json();
+      setNotices(res.data);
+      setTotalPages(res.meta.pagination.pageCount);
+      setLoading(false);
+    };
+    getData();
+  }, [currentPage]);
+
   const router = useRouter();
   const { setNews } = useNews();
 
@@ -275,83 +37,50 @@ function NewsPage() {
     setNews(newsItem);
     router.push("/Updates/Details");
   };
-  const [currentPage, setCurrentPage] = useState(1);
 
-  const totalPages = Math.ceil(newsData.length / ITEMS_PER_PAGE);
-
-  const handlePageChange = (page) => {
-    if (page > 0 && page <= totalPages) {
-      setCurrentPage(page);
-    }
-  };
-
-  const getCurrentPageData = () => {
-    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-    return newsData.slice(startIndex, startIndex + ITEMS_PER_PAGE);
-  };
-
-  const renderPageNumbers = () => {
-    const pageNumbers = [];
-    const maxPagesToShow = 5;
-    let startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2));
-    let endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
-
-    if (endPage - startPage < maxPagesToShow - 1) {
-      startPage = Math.max(1, endPage - maxPagesToShow + 1);
-    }
-
-    for (let i = startPage; i <= endPage; i++) {
-      pageNumbers.push(
-        <button
-          key={i}
-          onClick={() => handlePageChange(i)}
-          className={`px-4 py-2 rounded-md ${currentPage === i ? 'bg-sky-400' : 'bg-gray-300'} text-black`}
-        >
-          {i}
-        </button>
-      );
-    }
-
-    return pageNumbers;
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
   };
 
   return (
     <div className='flex flex-col bg-white py-5 min-h-dvh text-slate-600 text-right items-center'>
       <div className='pt-6 text-left flex justify-start max-w-[1256px] w-full px-9'>
-        <BreadCrumbs/>
+        <BreadCrumbs />
       </div>
       <div>
         <h1 className='text-5xl font-sans font-light relative right-6 text-sky-950'>NEWS</h1>
         <div className="py-6 text-left flex flex-col items-center max-w-[1256px] w-full px-4">
-          {getCurrentPageData().map((newsItem, index) => (
-            <div key={index} onClick={() => handleCardClick(newsItem)}>
-              <NewsCard
-                date={newsItem.date}
-                title={newsItem.title}
-                description={newsItem.description}
-                viewMoreLink={newsItem.viewMoreLink}
-                galleryLink={newsItem.galleryLink}
-              />
-            </div>
-          ))}
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            notices.map((newsItem) => {
+              const viewMoreLink = newsItem.attributes.Pdf?.data?.attributes?.url || "#";
+              const galleryLink = newsItem.attributes.Pdf?.data?.attributes?.url || "#";
+              return (
+                <div key={newsItem.id} onClick={() => handleCardClick(newsItem)}>
+                  <NewsCard
+                    date={newsItem.attributes.publishedAt}
+                    title={newsItem.attributes.Title}
+                    description={newsItem.attributes.Description}
+                    viewMoreLink={viewMoreLink}
+                    galleryLink={galleryLink}
+                  />
+                </div>
+              );
+            })
+          )}
         </div>
       </div>
-      <div className="flex flex-wrap gap-2 justify-center space-x-2 mb-10">
-        <button
-          onClick={() => handlePageChange(currentPage - 1)}
-          className={`px-4 py-2 rounded-md ${currentPage === 1 ? 'bg-gray-100' : 'bg-sky-300'} text-black`}
-          disabled={currentPage === 1}
-        >
-          {`<<`}
-        </button>
-        {renderPageNumbers()}
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          className={`px-4 py-2 rounded-md ${currentPage === totalPages ? 'bg-gray-100' : 'bg-sky-300'} text-black`}
-          disabled={currentPage === totalPages}
-        >
-           {`>>`}
-        </button>
+      <div className="flex justify-center mb-10">
+        <Stack spacing={2}>
+          <Pagination
+            count={totalPages}
+            page={currentPage}
+            onChange={handlePageChange}
+            variant="outlined"
+            color="primary"
+          />
+        </Stack>
       </div>
     </div>
   );
--- src/components/HomeComponents/NewsComponents/NewsCard.jsx
+++ src/components/HomeComponents/NewsComponents/NewsCard.jsx
@@ -7,6 +7,8 @@ import Link from 'next/link';
 
 function NewsCard({ date, title, description, viewMoreLink, galleryLink }) {
   const { ref, controls } = useAnimationHook();
+  console.log("View mor elink",viewMoreLink);
+  
 
   return (
 
@@ -17,22 +19,22 @@ function NewsCard({ date, title, description, viewMoreLink, galleryLink }) {
       className='px-5 py-5 text-xl font-sans font-light'
     >
      
-      <div className='flex flex-col sm:flex-row p-4 gap-4 items-center bg-transparent text-gray-600 rounded-lg shadow-md'>
+      <div className='flex flex-col sm:flex-row p-4 gap-4 items-start bg-transparent text-gray-600 rounded-lg shadow-md'>
         <div className='flex gap-4 items-center'>
-          <p>{date}</p>
           <div className='flex flex-col gap-2'>
+          <p className='w-max pt-2'>{(date.split('T'))[0]}</p>
             <Link href={viewMoreLink} className="bg-transparent border text-xl text-center border-gray-500 px-4 py-2 rounded w-max">View More</Link>
-            <Link href={galleryLink} className="bg-transparent border text-xl text-center border-gray-500  px-4 py-2 rounded">Gallery</Link>
+            
           </div>
         </div>
-        <Link href={`/News/${title}`}>
+        
         <div className='flex-grow hover:text-sky-500 transition duration-300 '>
           <h2 className='text-3xl mb-4 text-sky-800 '>{title}</h2>
           <p className='line-clamp-6 sm:line-clamp-4 overflow-hidden'>
             {description}
           </p>
         </div>
-        </Link>
+        
       </div>
       {/* <hr className='h-[1px] w-full bg-black text-transparent' /> */}
     
