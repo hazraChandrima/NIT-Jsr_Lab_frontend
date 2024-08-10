381d1ded952598b59d3b9ebd5f8a33c6a03c8706
DEVDUTTSAHOO30 <2023ugcs032@nitjsr.ac.in>
2024-08-10 17:56:38 +0530

    backend integration in the members/collaborators

--- src/app/(routes)/Members/(subroutes)/Collaborators/page.jsx
+++ src/app/(routes)/Members/(subroutes)/Collaborators/page.jsx
@@ -1,15 +1,13 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
-import StaffCard from "../../_components/StudentCard";
-
-
-
+import CollaboratorsCard from "../../_components/CollaboratorsCard";
 
 function Collaborators() {
-
-  const [collaborators,setCollaborators]= useState([]);
-  const[loading, setLoading]= useState(false);
+  const [collaborators, setCollaborators] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const imageUrl =
+    "https://i.pinimg.com/736x/09/21/fc/0921fc87aa989330b8d403014bf4f340.jpg";
 
   useEffect(() => {
     const getData = async () => {
@@ -23,46 +21,39 @@ function Collaborators() {
     };
 
     getData();
-
-    
   }, []);
-
-
 
   return (
     <main className="min-h-screen w-full flex flex-col items-center bg-white py-20 px-5">
       <div className="flex flex-col items-start max-w-[1256px]">
-        {
-          loading?<>
+        {loading ? (
           <div>Loading Data...</div>
-          </>:<>
-          {/* //Faculty */}
-        <div className=" mb-20">
-          {/* //heading */}
-          <div className="mb-10">
-            <h1 className="text-3xl mb-5 text-sky-950">
-              Collaborative Researchers
-            </h1>
-            <div className="w-full h-[2px] bg-slate-400">
-              <div className="w-[15%] h-full bg-sky-500"></div>
+        ) : (
+          <>
+            {/* Faculty */}
+            <div className="mb-20">
+              {/* Heading */}
+              <div className="mb-10">
+                <h1 className="text-3xl mb-5 text-sky-950">
+                  Collaborative Researchers
+                </h1>
+                <div className="w-full h-[2px] bg-slate-400">
+                  <div className="w-[15%] h-full bg-sky-500"></div>
+                </div>
+              </div>
+              <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                {collaborators.map((member) => (
+                  <CollaboratorsCard
+                    id={member.id}
+                    name={member.attributes.name}
+                    role={member.attributes.role}
+                    imageUrl={imageUrl}
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-          <div className=" w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {collaborators.map((member, index) => (
-              <StaffCard
-                key={member.id}
-                name={member.
-                  attributes.name}
-                role={member.
-                  attributes.role}
-                imageUrl={member.attributes.profilePhoto.data.attributes.url}
-              />
-            ))}
-          </div>
-        </div>
           </>
-        }
-        
+        )}
       </div>
     </main>
   );
---
+++ src/app/(routes)/Members/_components/CollaboratorsCard.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import useAnimationHook from "@/hooks/AnimationHooks/moveUp";
+import Link from "next/link";
+
+const CollaboratorsCard = ({ name, role, imageUrl, id }) => {
+  const { ref, controls } = useAnimationHook();
+
+  return (
+    <Link href={`/Members/Collaborators/${id}`}>
+      <motion.div
+        ref={ref}
+        initial={{ opacity: 0, y: 50 }}
+        animate={controls}
+        className="bg-white rounded-sm shadow-lg overflow-hidden cursor-pointer"
+      >
+        <div className="overflow-hidden flex items-center justify-center">
+          <img src={imageUrl} alt={name} className="h-full w-auto" />
+        </div>
+        <div className="p-3 bg-sky-600 w-[85%] relative bottom-4 text-white">
+          <p className="text-lg font-light truncate">{role}</p>
+        </div>
+        <div className="px-3">
+          <hr className="bg-slate-500 h-[1px]" />
+        </div>
+        <div className="px-4 py-2">
+          <div className="border-l-4 border-sky-600 px-2">
+            <p className="text-black font-sans text-left text-xl truncate">
+              {name}
+            </p>
+          </div>
+        </div>
+      </motion.div>
+    </Link>
+  );
+};
+
+export default CollaboratorsCard;
