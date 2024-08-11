cb744f03123f021ec346e010a6be09bacb58a50e
Chandrima Hazra <hazrachandrima6@gmail.com>
2024-08-11 18:13:10 +0530

    changed contrast in mobile navbar

--- src/components/MobileNavbar/mobileNavbar.jsx
+++ src/components/MobileNavbar/mobileNavbar.jsx
@@ -95,7 +95,7 @@ export const MobileNavbar = () => {
                         exit="exit"
                         variants={containerVariants}
                     >
-                        <div className="overflow-y-scroll font-sans pt-10 mt-auto mb-10 pb-8 text-white text-center h-auto w-full max-w-3xl">
+                        <div className="overflow-y-scroll font-sans pt-10 mt-auto mb-10 pb-8 text-slate-200 text-center h-auto w-full max-w-3xl">
                             {subroutes.map((section, index) => (
                                 <motion.div key={index}
                                     className="mb-6"
