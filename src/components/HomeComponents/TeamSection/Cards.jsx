bfb50f246704055d9d5e26b50ce97cdabb9a92e6
Chandrima Hazra <hazrachandrima6@gmail.com>
2024-08-12 16:38:34 +0530

    populated research project cards with thumbnail image from backend

--- src/components/HomeComponents/TeamSection/Cards.jsx
+++ src/components/HomeComponents/TeamSection/Cards.jsx
@@ -1,12 +1,12 @@
 "use client";
-
+// import Image from "next/image";
 import { useEffect, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import ViewMore from "../ViewMoreComponent/ViewMoreComponent";
 import Link from "next/link";
 
-const TeamComponent = ({ title, description, link }) => {
+const TeamComponent = ({ title, description, link, imageUrl}) => {
   const controls = useAnimation();
   const [ref, inView] = useInView({
     triggerOnce: true, // Only trigger once
@@ -29,16 +29,18 @@ const TeamComponent = ({ title, description, link }) => {
         ref={ref}
         initial={{ opacity: 0, y: 50 }} // Start slightly below with 0 opacity
         animate={controls}
-        className="relative h-[345px] w-full"
+        className="relative h-[345px] w-full filter grayscale hover:filter-none transition-all transition-duration-500"
       >
         <img
-          src="https://www.mccmdclinic.org/wp-content/uploads/2020/01/Microscope.jpg"
+          src={imageUrl}
           alt="Research Image"
-          className="h-full object-cover w-full filter grayscale hover:filter-none transition-all duration-400"
+          // width={250}
+          // height={500}
+          className="h-full object-cover w-full "
         />
 
         <div className="absolute top-2 left-0 p-4 text-slate-900">
-          <h2 className="text-3xl lg:text-2xl font-light font-sans">{title}</h2>
+          <h2 className="text-3xl lg:text-2xl font-light font-sans text-white">{title}</h2>
         </div>
         <div className="absolute bottom-0 right-3 ">
           {/* <button className="bg-blue-500 text-white px-4 py-2 rounded">View More</button> */}
--- src/components/HomeComponents/ViewMoreComponent/ViewMoreComponent.jsx
+++ src/components/HomeComponents/ViewMoreComponent/ViewMoreComponent.jsx
@@ -12,8 +12,8 @@ const ViewMore = ({ width }) => {
       onMouseLeave={() => setHovered(false)}
     >
       <div className="relative font-sans text-xl mt-5 font-light text-right">
-        <span className="mr-2 text-black ">View More</span>
-        <span className="text-2xl text-black">{hovered ? '-' : '+'}</span>
+        <span className="mr-2 text-white ">View More</span>
+        <span className="text-2xl text-white">{hovered ? '-' : '+'}</span>
       </div>
       <div
         className="relative h-[2px] rounded-xl overflow-hidden bg-slate-200 mt-4 mb-7 lg:mb-3 "
@@ -21,7 +21,7 @@ const ViewMore = ({ width }) => {
       >
         
         <div
-          className={`absolute rounded-xl top-0 left-0 h-full transition-all duration-300 ${hovered ? 'w-full bg-cyan-600' : 'w-1/5 bg-cyan-600'}`}
+          className={`absolute rounded-xl top-0 left-0 h-full transition-all duration-300 ${hovered ? 'w-full bg-sky-400' : 'w-1/5 bg-sky-400'}`}
         ></div>
       </div>
       
